1558bb6a561f125b824db7de7a7305eab1b84f26
Illia Matsykur <matsykur.illya@gmail.com>
2026-06-16 23:50:09 +0200

    added new api

--- games/urls.py
+++ games/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from  .views import AllCategoryView, AllGamesView
+from  .views import AllCategoryView, AllGamesView, GameSpecificView
 urlpatterns = [
     path('', AllGamesView.as_view(), name='games'),
-    path('categories/', AllCategoryView.as_view(), name='categories')
-
+    path('categories/', AllCategoryView.as_view(), name='categories'),
+    path('<slug>/', GameSpecificView.as_view(), name='game')
 ]
 
 # api/games/ - games
--- games/views.py
+++ games/views.py
@@ -12,3 +12,9 @@ class AllCategoryView(generics.ListAPIView):
     permission_classes = []
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class GameSpecificView(generics.RetrieveAPIView):
+    permission_classes = []
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    lookup_field = 'slug'
