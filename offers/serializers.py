255da605197556f6fadcaac72b38d2d3860bce1a
Illia Matsykur <matsykur.illya@gmail.com>
2026-06-21 19:19:24 +0200

    fix

--- offers/serializers.py
+++ offers/serializers.py
@@ -7,3 +7,24 @@ class OfferSerializer(serializers.ModelSerializer):
         model = Offer
         fields = ('uuid', 'title', 'description', 'price', 'stock', 'is_active', 'is_auto_delivery', 'created_at', 'custom_field', 'seller', 'game',  'category', 'secret_data')
         extra_kwargs = {'secret_data' : {'write_only' : True}}
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.seller:
+            representation['seller'] = {
+                'id': instance.seller.id,
+                'username' : instance.seller.username,
+
+            }
+        if instance.game:
+            representation['game'] = {
+                'id' : instance.game.id,
+                'title' : instance.game.title,
+                'slug' : instance.game.slug
+            }
+        if instance.category:
+            representation['category'] = {
+                'id' : instance.category.id,
+                'title' : instance.category.title,
+                'slug' : instance.category.slug
+            }
+        return representation
